162b49fb47a83abbf423756962d8166ad7e001e5
Tim Mandel <timothy.mandel@gmail.com>
2019-03-07 12:52:07 -0600

    can login

--- src/AuthContainer/login.js
+++ src/AuthContainer/login.js
@@ -5,12 +5,54 @@ class Login extends Component {
 		super();
 
 		this.state = {
-
+			username: '',
+			password: '',
+			message: ''
+		}
+	}
+	handleChange = (e)=>{
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+	handleSubmit = async (e)=>{
+		e.preventDefault();
+		try{
+			const loginResponse = await fetch(`${process.env.REACT_APP_ROUTE}auth/login`, {
+				method: "POST",
+				credentials: 'include',
+				body: JSON.stringify(this.state),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+			if(!loginResponse.ok){
+				this.setState({
+					message: "Incorrect username or password"
+				})
+				throw Error(loginResponse.statusText);
+			}
+			const parsed = await loginResponse.json();
+			console.log(parsed);
+			if(parsed){
+				this.setState({
+					message: ''
+				})
+			}
+		}catch(err){
+			console.log(err);
+			return(err);
 		}
 	}
 	render() {
 		return (
-			<div>LOGIN</div>
+			<div>LOGIN
+				<form className="login-form" onSubmit={this.handleSubmit}>
+					<input type="text" name="username" placeholder="Username" onChange={this.handleChange} className="login-input"/>
+					<input type="password" name="password" placeholder="Password" onChange={this.handleChange} className="login-input"/>
+					<button>Log In</button>
+				</form>
+			</div>
 		)
 	}
 }
