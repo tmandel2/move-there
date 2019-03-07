3f4683e190a406f423de74e9b978665d52eb2da8
Tim Mandel <timothy.mandel@gmail.com>
2019-03-07 14:24:45 -0600

    Address Container made and routed

---
+++ src/AddressContainer/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class AddressContainer extends Component {
+	constructor(){
+		super();
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+			<div>THIS IS AN AddressContainer
+			</div>
+		)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+export default withRouter(AddressContainer);
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import './App.css';
 import AuthContainer from './AuthContainer';
 import UserContainer from './UserContainer';
+import AddressContainer from './AddressContainer';
 
 const My404 = () => {
   return (
@@ -37,6 +38,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" render= {props => <AuthContainer username={this.state.username} _id={this.state._id} logIn={this.logIn} history={this.props.history} />} />
           <Route exact path="/users" render={props => <UserContainer username={this.state.username} _id={this.state._id} history={this.props.history} />} />
+          <Route exact path="/addresses" render={props => <AddressContainer username={this.state.username} _id={this.state._id} history={this.props.history} />} />
           <Route component={ My404 }/>
         </Switch>
       </main>
