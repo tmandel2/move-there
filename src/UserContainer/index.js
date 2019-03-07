11f117b74bf2b619c98a6e2b4dd12c560537c0e8
Tim Mandel <timothy.mandel@gmail.com>
2019-03-07 15:31:59 -0600

    User show page get route works

--- src/UserContainer/index.js
+++ src/UserContainer/index.js
@@ -6,12 +6,35 @@ class UserContainer extends Component {
 		super();
 
 		this.state = {
+			user: {}
 			
+		}
+	}
+	componentDidMount() {
+		this.getUser();
+	}
+	getUser = async () => {
+		try {
+			const response = await fetch(`${process.env.REACT_APP_ROUTE}users/${this.props._id}`, {
+				credentials: 'include'
+			});
+			if(!response.ok){
+				throw Error(response.statusText);
+			}
+			const userParsed = await response.json();
+			console.log(userParsed);
+			this.setState({
+				user: userParsed.user
+			})
+		} catch(err) {
+			console.log(err);
+			return err;
 		}
 	}
 	render() {
 		return (
 			<div>User Container Found
+			{this.state.user.username}
 			</div>
 		)
 	}
