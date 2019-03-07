2c582901347aea202767a8d17eaa083662a60cac
Tim Mandel <timothy.mandel@gmail.com>
2019-03-07 13:05:54 -0600

    User container and route added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import './App.css';
 import AuthContainer from './AuthContainer';
+import UserContainer from './UserContainer';
 
 const My404 = () => {
   return (
     <div>
-      <img src="404Cat.jpg"/>
+      <img src="404Cat.jpg" alt="404 Cat"/>
     </div>
   )
 }
@@ -23,6 +24,7 @@ class App extends Component {
       <main>
         <Switch>
           <Route exact path="/" component={ AuthContainer }/>
+          <Route exact path="/users" component={ UserContainer }/>
           <Route component={ My404 }/>
         </Switch>
       </main>
---
+++ src/UserContainer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class UserContainer extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			
+		}
+	}
+	render() {
+		return (
+			<div>User Container Found
+			</div>
+		)
+	}
+}
+
+
+export default UserContainer;
