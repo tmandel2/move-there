e0ae4293c0db3a1ca1ba8b8d4bc64bc130bcc08a
Tim Mandel <timothy.mandel@gmail.com>
2019-03-08 12:21:04 -0600

    youtube video embeded

--- src/AddressContainer/AddressShow.js
+++ src/AddressContainer/AddressShow.js
@@ -6,19 +6,32 @@ class AddressShow extends Component {
 	constructor() {
 		super();
 		this.state = {
+			youTubeURL: ''
 		}
 	}
 	componentDidMount() {
-		this.getAddress();
+		this.getYouTube();
 	}
-	getAddress = () => {
+	getYouTube = async () => {
+		try{
+			const youtubeResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoEmbeddable=true&q=${this.props.currentAddress.city}+${this.props.currentAddress.state}+tour&safeSearch=moderate&order=relevance&key=${process.env.REACT_APP_GOOGLEKEY}`);
 
+			const parsedYoutube = await youtubeResponse.json();
+
+			this.setState({
+				youTubeURL: `https://www.youtube.com/embed/${parsedYoutube.items[0].id.videoId}`
+			})
+
+		} catch(err) {
+			console.log(err);
+		}
 	}
 	render() {
 		return (
 			<div>
 				Address AdressShow
 				{this.props.currentAddress.id} {this.props.currentAddress.walkScore}{this.props.currentAddress.streetNumber} {this.props.currentAddress.streetName}, {this.props.currentAddress.city}, {this.props.currentAddress.state} {this.props.currentAddress.zipCode}
+				<iframe width="300" height="200" src={this.state.youTubeURL}></iframe>
 			</div>
 		)
 	}
