e48f14bf904dd07511a51b62de1138c8c9de351f
Tim Mandel <timothy.mandel@gmail.com>
2019-03-08 10:26:13 -0600

    Addresses index shows information from all addresses

--- src/AddressContainer/AddressIndex.js
+++ src/AddressContainer/AddressIndex.js
@@ -9,13 +9,44 @@ class AddressIndex extends Component {
 		super();
 
 		this.state = {
-
+			addresses: []
+		}
+	}
+	componentDidMount() {
+		this.getAddresses();
+	}
+	getAddresses = async () => {
+		try {
+			const response = await fetch(`${process.env.REACT_APP_ROUTE}addresses`, {
+				credentials: 'include'
+			});
+			if(!response.ok){
+				throw Error(response.statusText);
+			}
+			const addressesParsed = await response.json();
+			console.log(addressesParsed);
+			this.setState({
+				addresses: addressesParsed
+			})
+		} catch(err) {
+			console.log(err);
+			return err;
 		}
 	}
 	render() {
+		const addressesList = this.state.addresses.map((address, i) => {
+			if(address) {
+				return <li key={i}>
+					{address.streetNumber} {address.streetName}, {address.city}, {address.state} {address.zipCode}
+				</li>
+			} else {
+				return null
+			}
+		})
 		return(
 			<div>
 				This is address AddressIndex
+				{addressesList}
 			</div>
 		)
 	}
